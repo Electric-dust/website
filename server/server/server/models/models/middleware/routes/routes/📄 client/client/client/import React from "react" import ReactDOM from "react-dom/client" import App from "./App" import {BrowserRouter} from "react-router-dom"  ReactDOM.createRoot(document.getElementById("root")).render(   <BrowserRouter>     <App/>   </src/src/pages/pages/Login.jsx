716e881201cb5d088240250290cbf6b76c7f80f2
Electric-dust <mahadin8@gmail.com>
2026-02-25 20:42:53 +0600

    Create Login.jsx

---
+++ server/server/server/models/models/middleware/routes/routes/📄 client/client/client/import React from "react" import ReactDOM from "react-dom/client" import App from "./App" import {BrowserRouter} from "react-router-dom"  ReactDOM.createRoot(document.getElementById("root")).render(   <BrowserRouter>     <App/>   </src/src/pages/pages/Login.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react"
+import axios from "axios"
+
+const API = "http://localhost:5000/api"
+
+export default function Login(){
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+
+  const handleLogin=async()=>{
+    const res = await axios.post(`${API}/auth/login`,{email,password})
+    localStorage.setItem("token",res.data.token)
+    alert("Login Successful")
+  }
+
+  return(
+    <div>
+      <h1>Login</h1>
+      <input placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+      <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
